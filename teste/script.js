eeed125abf42ef8a7408a013532cc24ca010eba4
GabrielGimenes <gabrieleduardogimenesdacruz@gmail.com>
2025-01-16 13:10:01 -0300

    Melhoras

---
+++ teste/script.js
@@ -0,0 +1,23 @@
+// Abrir o modal
+/*function openModal(day) {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'flex';
+  
+    const form = document.getElementById('serviceForm');
+    form.reset(); // Limpar o formulário
+  
+    form.onsubmit = (e) => {
+      e.preventDefault();
+      const serviceName = document.getElementById('serviceName').value;
+      const serviceTime = document.getElementById('serviceTime').value;
+      alert(`Serviço "${serviceName}" marcado para o dia ${day} às ${serviceTime}`);
+      closeModal();
+    };
+  }
+  
+  // Fechar o modal
+  function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+  }*/
+  
